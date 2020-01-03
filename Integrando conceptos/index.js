3a6f59de8d55d9599987382d78764dbdff909708
nikolasgamertv@gmail.com <nikolasgamertv@gmail.com>
2020-01-03 18:33:09 -0500

    conceptos

--- Integrando conceptos/index.js
+++ Integrando conceptos/index.js
@@ -1,6 +1,6 @@
 var subject = new rxjs.Subject();
 var { Observable, interval, fromEvent, of, from, pipe } = rxjs
-var { map, take } = rxjs.operators
+var { map, take, first } = rxjs.operators
 
 // const obeservable = new Observable(observer => {
 //     observer.next(1)
@@ -26,12 +26,18 @@ var { map, take } = rxjs.operators
 //     }
 // })
 
-const observable = new Observable((subscriber) => {
-    const id = setInterval(() => {
-        subscriber.next('hi')
-    }, 1000);
-})
+// const observable = new Observable((subscriber) => {
+//     const id = setInterval(() => {
+//         subscriber.next('hi')
+//     }, 1000);
+// })
 
-observable.subscribe((x) => {
-    console.log(x)
-})
+// observable.subscribe((x) => {
+//     console.log(x)
+// })
+
+const numeros = of(1, 2, 3, 4, 5)
+numeros.pipe(map(x => x * x))
+    .subscribe(resp => {
+        console.log(resp)
+    })
